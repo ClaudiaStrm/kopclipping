becdf1fc2b5ee056069a6b954c47c7cdd2877470
Claudia Strm <s_moura@hotmail.com>
2017-06-26 20:44:37 -0300

    Adicionar chaves de acesso

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 var TwitterPackage = require('twitter');
 
 var secret = {
-  consumer_key: 'YOURS',
-  consumer_secret: 'YOURS',
-  access_token_key: 'YOURS',
-  access_token_secret: 'YOURS'
+  consumer_key: 'gVHf3LcfleMvGCFVb1YGRoOsB',
+  consumer_secret: '4JEAxA0zU9qwOpOk52oK11FPoifZ434W716V64QVFUL5xCZLLb',
+  access_token_key: '120516404-U7hYSJ0VIJJd7XN6Ym7FxUYv6DXJhAKJ0BBoxwT1',
+  access_token_secret: 'VcaW4JBTHzoQd7N3w4P0F6vZ0qoEjqkbmWdVgN8KaJ8HE'
 }
 
 var Twitter = new TwitterPackage(secret);
 
-Twitter.stream('statuses/filter', {track: '@kopzinksi'}, function(stream) {
+Twitter.stream('statuses/filter', {track: 'Câmara'}, function(stream) {
   stream.on('data', function(tweet) {
     console.log(tweet.text);
   });
